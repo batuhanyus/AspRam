353372d586d7f6e98ea9bf7230f224f7a98ea899
Batuhan Avcı <batuhanavcix@gmail.com>
2023-01-24 23:01:21 +0300

    Minimale.

---
+++ Minimale/Program.cs
@@ -0,0 +1,10 @@
+var builder = WebApplication.CreateBuilder(args);
+var app = builder.Build();
+
+app.MapGet("/", () => "Hello World!");
+
+app.MapGet("/ram", () => $"{GC.GetTotalMemory(false) / 1024 / 1024} MB (managed)"
+                         + $"\r" +
+                         $"System: {Environment.WorkingSet / 1024 / 1024} MB (total)");
+
+app.Run();
