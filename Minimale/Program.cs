af3a031956f1cd227b0e8e5d484e83adb9dee50a
Batuhan Avcı <batuhanavcix@gmail.com>
2023-01-25 09:50:37 +0300

    Update Program.cs

--- Minimale/Program.cs
+++ Minimale/Program.cs
@@ -5,10 +5,7 @@ var app = builder.Build();
 
 app.Urls.Add("http://*:5000");
 
-
-app.MapGet("/", () => "Hello World!");
-
-app.MapGet("/ram", () => $"{GC.GetTotalMemory(false) / 1024 / 1024} MB (managed)"
+app.MapGet("/", () => $"{GC.GetTotalMemory(false) / 1024 / 1024} MB (managed)"
                          + Environment.NewLine +
                          $"System: {Environment.WorkingSet / 1024 / 1024} MB (total)"
                          + Environment.NewLine +
